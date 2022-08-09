5f085a6d5e936bdc6f7a6d32688f36269ab88b71
angel <angelomelo_tdai@hotmail.com>
2022-08-09 15:06:44 -0300

    Primeiro commit

--- selects.sql
+++ selects.sql
@@ -2,11 +2,50 @@
 -- Seleções de dados
 
 -- 1)
+    SELECT
+        *
+    FROM
+        pedidos p
+    JOIN 
+        produtos pp ON TRUE;
 
 -- 2)
+    SELECT
+        pedido_id
+    FROM
+        produtos_pedidos
+    JOIN
+        produtos ON produtos.id = produtos_pedidos.produto_id AND produtos.nome = 'Fritas';
 
 -- 3)
+    SELECT
+	    clientes.nome
+    FROM
+        clientes
+    JOIN
+        pedidos ON pedidos.cliente_id = clientes.id
+	JOIN
+    	produtos_pedidos ON produtos_pedidos.pedido_id = pedidos.id
+    JOIN
+        produtos ON produtos.id = produtos_pedidos.produto_id AND produtos.nome = 'Fritas';
 
 -- 4)
+    SELECT
+    	SUM(produtos.preco)
+    FROM
+    	produtos
+    JOIN
+    	produtos_pedidos ON produtos_pedidos.produto_id = produtos.id
+    JOIN
+    	pedidos ON pedidos.id = produtos_pedidos.pedido_id
+    JOIN
+    	clientes ON clientes.id = pedidos.cliente_id AND clientes.nome = 'Laura';
 
 -- 5)
+    SELECT
+		COUNT(produtos.id),
+        produtos.nome
+    FROM
+    	produtos_pedidos
+    JOIN
+    	produtos ON produtos_pedidos.produto_id = produtos.id GROUP BY (produtos.nome);
